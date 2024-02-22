28c57f4f3b6afddc7d7fa0c70d1aa2cb7d1c5863
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-22 10:51:02 +0430

    adding some functionality

--- app.js
+++ app.js
@@ -34,6 +34,12 @@ window.addEventListener("scroll", function(){
     }else{
         navbar.classList.remove("fixed-nav")
     };
+
+    if(scrollHeight > 500){
+        topLink.classList.add("show-link");
+    }else{
+        topLink.classList.remove("show-link")
+    }
 });
 
 // ********** smooth scroll ************
