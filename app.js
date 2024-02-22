0fbe75627b02bd5b23cc3882157aec183cee36f6
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-22 09:05:54 +0430

    adding some changes

--- app.js
+++ app.js
@@ -7,7 +7,8 @@
 const date = document.getElementById('date');
 date.innerHTML = new Date().getFullYear();
 // ********** close links ************
-
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
 // ********** fixed navbar ************
 
 // ********** smooth scroll ************
